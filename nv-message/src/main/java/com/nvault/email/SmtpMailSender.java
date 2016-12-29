d82e3978ebfd35be69a89dc3b4053191f40de059
nisumconsulting <nisumconsulting@10.0.0.55>
2016-12-29 20:14:11 +0530

    removing db interactions code

--- nv-message/src/main/java/com/nvault/email/SmtpMailSender.java
+++ nv-message/src/main/java/com/nvault/email/SmtpMailSender.java
@@ -8,8 +8,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
-import com.nvault.message.model.Message;
-import com.nvault.message.repository.MessageRepository;
 
 @Component
 public class SmtpMailSender {
@@ -17,8 +15,7 @@ public class SmtpMailSender {
 	@Autowired
 	private JavaMailSender javaMailSender;
 	
-	@Autowired
-	private MessageRepository messageRepository;
+	
 	
 	public void send(String[] toAddress, String subject,String body) throws Exception{
 		
@@ -41,13 +38,10 @@ public class SmtpMailSender {
 
 			javaMailSender.send(message);
 			
-			Message msg=new Message();
-			msg.setBody(body);
-			msg.setSubject(subject);
-			msg.setDate(new java.util.Date());
 			
 			
-			messageRepository.saveAndFlush(msg);
+			
+			
 			
 		} catch (Exception e) {
 
