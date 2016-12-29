081051810e76b65a9767842c248d28589570e2b3
nisumconsulting <nisumconsulting@10.0.0.55>
2016-12-29 19:03:55 +0530

    db insert of the email details

--- nv-message/src/main/java/com/nvault/email/SmtpMailSender.java
+++ nv-message/src/main/java/com/nvault/email/SmtpMailSender.java
@@ -8,11 +8,17 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import com.nvault.message.model.Message;
+import com.nvault.message.repository.MessageRepository;
+
 @Component
 public class SmtpMailSender {
 
 	@Autowired
 	private JavaMailSender javaMailSender;
+	
+	@Autowired
+	private MessageRepository messageRepository;
 	
 	public void send(String[] toAddress, String subject,String body) throws Exception{
 		
@@ -34,6 +40,14 @@ public class SmtpMailSender {
 			helper.setText(body,true);
 
 			javaMailSender.send(message);
+			
+			Message msg=new Message();
+			msg.setBody(body);
+			msg.setSubject(subject);
+			msg.setDate(new java.util.Date());
+			
+			
+			messageRepository.saveAndFlush(msg);
 			
 		} catch (Exception e) {
 
