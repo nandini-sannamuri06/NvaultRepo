277e6c0e05cb294d1df18f97557fcd4bc89eb107
Akalya Jayaraj <ajayaraj@nisum.com>
2016-12-30 07:16:58 +0000

    ContactApplication.java edited online with Bitbucket

--- nv-contact/src/main/java/com/nvault/ContactApplication.java
+++ nv-contact/src/main/java/com/nvault/ContactApplication.java
@@ -19,6 +19,14 @@ public class ContactApplication {
 	@Configuration
 	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		
+		@Override
+		  public void configure(WebSecurity web) throws Exception {
+		    web
+		      .ignoring()
+		         .antMatchers("/resource/**"); // #3
+		  }
+		
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			// @formatter:off
@@ -26,6 +34,8 @@ public class ContactApplication {
 				.httpBasic().and()
 				.authorizeRequests()
 					.anyRequest().permitAll();
+			http
+			  .csrf().disable();
 			// @formatter:on
 		}
 	}
