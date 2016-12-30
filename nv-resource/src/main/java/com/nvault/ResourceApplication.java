b9df812856a6ec0fbc55bc9aa07f12c51f5de351
Akalya Jayaraj <ajayaraj@nisum.com>
2016-12-30 07:16:03 +0000

    ResourceApplication.java edited online with Bitbucket

--- nv-resource/src/main/java/com/nvault/ResourceApplication.java
+++ nv-resource/src/main/java/com/nvault/ResourceApplication.java
@@ -20,6 +20,14 @@ public class ResourceApplication{
 	@Configuration
 	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		
+		@Override
+		  public void configure(WebSecurity web) throws Exception {
+		    web
+		      .ignoring()
+		         .antMatchers("/resource/**"); // #3
+		  }
+		
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			// @formatter:off
@@ -27,6 +35,8 @@ public class ResourceApplication{
 				.httpBasic().and()
 				.authorizeRequests()
 					.anyRequest().permitAll();
+			http
+			  .csrf().disable();
 		}
 	}
 }
