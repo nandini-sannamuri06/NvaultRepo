3f9a721ad0094401e1b81bd6b087faac2b1d3c18
akalya <nisumconsulting@10.0.0.37>
2016-12-30 14:15:25 +0530

    imports added

--- nv-resource/src/main/java/com/nvault/ResourceApplication.java
+++ nv-resource/src/main/java/com/nvault/ResourceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
