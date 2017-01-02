7cf383471a5e13a7a62a6b375d0b213429721623
Akalya Jayaraj <ajayaraj@nisum.com>
2017-01-02 10:34:11 +0000

    ContactController.java edited online with Bitbucket

--- nv-resource/src/main/java/com/nvault/contact/controller/ContactController.java
+++ nv-resource/src/main/java/com/nvault/contact/controller/ContactController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nvault.contact.model.Contact;
 import com.nvault.contact.service.ContactService;
+import com.nvault.model.NVaultUser;
 
 @RestController
 public class ContactController {
@@ -25,7 +28,10 @@ public class ContactController {
 
 	    @RequestMapping(value = "/create", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	    public ResponseEntity<Contact> registration(@RequestBody Contact contact) {
-	 		contact.setUserId("1");
+	    	
+	    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		    NVaultUser user = (NVaultUser)auth.getPrincipal();
+	 		contact.setUserId(user.getId()+"");
 	 		Contact contactResp = contactService.saveContact(contact);
 	    	return new ResponseEntity<Contact> (contactResp,HttpStatus.OK);
 	    }
